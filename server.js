d312136bba113d1eb87c2738a14fbaff18555fdb
Jae(Andrew) Kim <jae.m.kim@wsu.edu>
2020-07-26 21:15:35 -0700

    updates to upload to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/resume-website'));
+app.use(express.static(__dirname + '/dist/ResumeWebsite'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/resume-website/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/ResumeWebsite/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
